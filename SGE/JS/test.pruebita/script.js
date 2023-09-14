f5401ad2033b1784e6062e217615c81f64b3f92c
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2023-09-14 14:21:48 +0200

    sge: añado prueba con css y js: div, querySelector y addEventListener

---
+++ SGE/JS/test.pruebita/script.js
@@ -0,0 +1,5 @@
+let divBox1 = document.querySelector("#box1");
+divBox1.innerHTML = "Hello world";
+divBox1.addEventListener("click", function(){
+  alert("Clicked on the aquamarine background!");
+});
