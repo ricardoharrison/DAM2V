c7e811d2e4c41a6bf3ada841ba088ce34fcda3f8
at1DAM2 <at1DAM2@informatica.iesjc>
2023-09-20 17:55:52 +0200

    psp: añado primeras pruebas con C

---
+++ PSP/EjerciciosClase/ut01/io.c/number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int num = 43;
+    printf("Esto es un núemro: %d\n", num);
+    scanf("%d", &num);
+    printf("El número nuevo es: %d\n", num);
+    return 0;
+}
+
+//tarea: programa que le pida a un usuario la suma de dos números y devuelva el resultado
