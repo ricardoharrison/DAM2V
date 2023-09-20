3a8477f315efa4b909b22927768192fc9f918d75
at1DAM2 <at1DAM2@informatica.iesjc>
2023-09-20 21:31:21 +0200

    sge: añado ejercicios con appendChild, createElement, etc.

--- SGE/JS/test.pruebita/script.js
+++
@@ -1,5 +0,0 @@
-let divBox1 = document.querySelector("#box1");
-divBox1.innerHTML = "Hello world";
-divBox1.addEventListener("click", function(){
-  alert("Clicked on the aquamarine background!");
-});
