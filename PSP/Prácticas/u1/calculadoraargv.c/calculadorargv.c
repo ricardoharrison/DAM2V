c0a7e029f9ac6ea8c7c876083e433f533585dc3d
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2023-10-04 12:56:25 +0200

    psp: añado ejercicio calculadora argv

---
+++ PSP/Prácticas/u1/calculadoraargv.c/calculadorargv.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    
+    const int ARGUMENTOS_NECESARIOS = 4;
+    float resultado;
+    int opcionSwitch;
+   
+    if (argc < ARGUMENTOS_NECESARIOS) {
+        printf("No se han indicado suficientes argumentos.\n");
+    } else if (argc > ARGUMENTOS_NECESARIOS) {
+        printf("Se han indicado demasiados argumentos.\n");
+    } else {
+        if(strcmp(argv[1], "suma") == 0){
+        opcionSwitch = 1;
+        }
+        if(strcmp(argv[1], "resta") == 0){
+            opcionSwitch = 2;
+        }
+        if(strcmp(argv[1], "multiplicacion") == 0){
+            opcionSwitch = 3;
+        }
+        if(strcmp(argv[1], "division") == 0){
+            opcionSwitch = 4;
+        }
+        switch (opcionSwitch){
+            case 1:
+                resultado = atof(argv[2]) + atof(argv[3]);
+                printf("El resultado de la suma es %f\n", resultado);
+                break;
+            case 2:
+                resultado = atof(argv[2]) - atof(argv[3]);;
+                printf("El resultado de la resta es %f\n", resultado);
+                break;
+            case 3:
+                resultado = atof(argv[2]) * atof(argv[3]);
+                printf("El resultado de la multiplicación es %f\n", resultado);
+                break;
+            case 4:
+                if(argv[3] != 0){
+                    resultado = atof(argv[2]) / atof(argv[3]);
+                    printf("El resultado de la división es %f\n", resultado);
+                } else {
+                    printf("No es posible dividir entre 0\n");
+                }
+                break;
+            default:
+                printf("No se ha indicado ninguna orden válida\n");
+        }
+    }
+
+    return 0;
+}
