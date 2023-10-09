5789616feaded4b6a997f5b3aa3efa5a1cce2a0f
at1DAM2 <at1DAM2@informatica.iesjc>
2023-10-09 18:06:37 +0200

    psp: añado ejercio buclefork

---
+++ PSP/EjerciciosClase/u1/buclefork.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int printTable(int i){
+    printf("Tabla del %d   -> \t", i);
+    for (int j = 1; j <= 10; j++){
+        printf("%dx%d=%d\t", i, j, i*j);
+    }
+    printf("\n");
+    return 0;
+}
+
+int main(void){
+    for(int i = 1; i <= 10; i++){
+        pid_t idhijo;
+        idhijo = fork();
+        if(idhijo == 0){
+            printTable(i);
+            exit(0);
+        } else {
+            wait(NULL);
+        }
+    }
+    printf("FIN");
+}
