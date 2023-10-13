0bb3a2a91740927ba28004a61d8ba1897f487f99
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2023-10-13 21:17:10 +0200

    ad: añado ejercicos tema1 (9-25)

--- AD/ActividadesTema1/actividadesTema1.sql
+++ AD/ActividadesTema1/actividadesTema1.sql
@@ -221,6 +221,278 @@ EXCEPTION
 END;
 /
 
+--ejercicio10
+DROP TABLE temp;
+CREATE TABLE temp (
+    employee_id NUMBER,
+    old_job_id NUMBER,
+    new_job_id NUMBER
+);
+CREATE PROCEDURE CambiarOficio(in_employee_id NUMBER, in_new_job_id NUMBER)
+IS
+old_job_id NUMBER;
+BEGIN
+    SELECT job_id INTO old_job_id
+    FROM employee
+    WHERE employee_id = in_employee_id;
+
+    INSERT INTO temp (employee_id, old_job_id, new_job_id)
+    VALUES (in_employee_id, old_job_id, in_new_job_id);
+
+    UPDATE employee
+    SET job_id = in_new_job_id
+    WHERE employee_id = in_employee_id;
+END;
+/
+
+BEGIN
+    CambiarOficio(7369, 670);
+END;
+/
+
+--ejercicio11
+create or replace function calcSalarioAnual(salarioMensual number, comision number)
+return number
+is
+salarioAnual number;
+begin
+   salarioAnual := (nvl(salarioMensual, 0) * 12) + (nvl(comision, 0) * 12);
+   return salarioAnual;
+end;
+/
+
+--ejercicio13
+create table funciones (
+    funcion varchar2(50)
+);
+insert into funciones values('Enfermeras');
+insert into funciones values('Médicos');
+insert into funciones values('Conserjes');
+insert into funciones values('Limpiadores');
+select * from funciones;
+create or replace trigger funciones_trigger
+after update on funciones
+begin
+  dbms_output.put_line('Se ha hecho un cambio en la tabla FUNCIONES');
+end;
+/
+
+--ejercicio14
+CREATE TABLE cambios_detalles (
+  usuario VARCHAR2(50),
+  fecha_modificacion DATE,
+  codigo_pedido NUMBER,
+  mensaje VARCHAR2(100)
+);
+
+CREATE OR REPLACE TRIGGER detalles_trigger
+AFTER INSERT OR UPDATE OR DELETE ON detalles
+FOR EACH ROW
+DECLARE
+  v_mensaje VARCHAR2(100);
+BEGIN
+  IF INSERTING THEN
+    v_mensaje := 'Detalle dado de alta';
+  ELSIF UPDATING THEN
+    v_mensaje := 'Detalle modificado';
+  ELSIF DELETING THEN
+    v_mensaje := 'Detalle borrado';
+  END IF;
+
+  INSERT INTO cambios_detalles (usuario, fecha_modificacion, codigo_pedido, mensaje)
+  VALUES (USER, SYSDATE, :OLD.codigo_pedido, v_mensaje);
+END;
+/
+
+
+--ejercicio15
+drop table temp;
+create table temp(
+  emp_id number,
+  emp_name varchar2(50),
+  descrip varchar2(50)
+);
+create or replace trigger subida_salario after update on employee
+for each row
+begin
+    if :old.salary < :new.salary then
+        insert into temp values (:new.employee_id, :new.last_name, 'Subida de salario');
+    end if;
+end;
+/
+
+--ejercicio16
+create or replace trigger borrado_empleado after delete on employee
+for each row
+begin
+    insert into temp values (:old.employee_id, :old.last_name, :old.department_id);
+end;
+/
+
+--ejercicio18
+DROP TABLE temp;
+CREATE TABLE temp(cod NUMBER, msg VARCHAR2(50));
+DECLARE
+    cod_cliente customer.customer_id%type;
+    existe number;
+BEGIN
+cod_cliente :=  '&cod_cliente';
+select count(*) into existe from customer where customer_id = to_number(cod_cliente);
+    IF existe = 1 THEN
+        INSERT INTO temp VALUES (TO_NUMBER(cod_cliente), 'EXISTE');
+    ELSE
+        INSERT INTO temp VALUES (TO_NUMBER(cod_cliente), 'NO EXISTE');
+    END IF;    
+EXCEPTION
+ WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END;
+/
+
+--ejercicio19
+--este no se puede hacer con la BBDD que manejamos
+
+--ejercicio20
+DROP TABLE temp;
+CREATE TABLE temp (
+    id_cliente NUMBER,
+    nombre VARCHAR2(50),
+    pedidos NUMBER,
+    total NUMBER
+);
+DECLARE
+    codigo customer.customer_id%TYPE := '&codigo';
+    nombre customer.name%TYPE;
+    pedidos NUMBER;
+    total sales_order.total%TYPE;
+    existe_cliente NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO existe_cliente FROM customer WHERE customer_id = codigo;
+    SELECT COUNT(*) INTO existe_pedido FROM sales_order WHERE customer_id = codigo;
+    IF existe_cliente != 1 THEN
+        RAISE_APPLICATION_ERROR(-200001, 'No existe ese cliente');
+    ELSIF existe_pedido < 1 THEN
+        RAISE_APPLICATION_ERROR(-200002, 'No existen pedidos de ese cliente');
+    ELSE
+        SELECT customer_id, name INTO codigo, nombre FROM customer WHERE customer_id = codigo;
+        SELECT COUNT(*) INTO pedidos FROM sales_order WHERE customer_id = codigo;
+        SELECT SUM(total) INTO total FROM sales_order WHERE customer_id = codigo;
+        INSERT INTO temp VALUES(codigo, nombre, pedidos, total);
+    end if;
+end;
+/
+
+--ejercicio21
+create or replace trigger borrado_empleado after delete on employee
+for each row
+begin
+    dbms_output.put_line('Empleado ' || :old.last_name || ' con número ' || :old.employee_id || ' eliminado');
+end;
+/
+
+--ejercicio22
+CREATE OR REPLACE PROCEDURE modificar_salario(p_employee_id IN employee.employee_id%TYPE) IS
+    v_employee_count NUMBER;
+    v_job_id employee.job_id%TYPE;
+    v_function job.function%TYPE;
+    v_salary_increase NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_employee_count
+    FROM employee
+    WHERE manager_id = p_employee_id;
+    
+    SELECT function
+    INTO v_function
+    FROM job
+    WHERE job_id = (SELECT job_id FROM employee WHERE employee_id = p_employee_id);   
+
+    IF v_function = 'PRESIDENT' THEN
+        v_salary_increase := 30;
+    ELSIF v_employee_count = 0 THEN
+        v_salary_increase := 50;
+    ELSIF v_employee_count = 1 THEN
+        v_salary_increase := 80;
+    ELSIF v_employee_count = 2 THEN
+        v_salary_increase := 100;
+    ELSE
+        v_salary_increase := 110;
+    END IF;
+
+    UPDATE employee
+    SET salary = salary + v_salary_increase
+    WHERE employee_id = p_employee_id;
+
+END;
+/
+
+--ejercicio23
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Hola mundo');
+END;
+/
+DECLARE
+    texto VARCHAR2(20) := 'Hola mundo';
+    texto_al_reves VARCHAR2(20);
+BEGIN
+    FOR i IN REVERSE 1..LENGTH(texto)
+    LOOP
+        texto_al_reves := texto_al_reves || SUBSTR(texto, i, 1);
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE(texto_al_reves);
+END;
+/
+
+--ejercicio24
+DECLARE
+    emp_name VARCHAR2(50);
+    manager_name VARCHAR2(50);
+    
+    CURSOR emp_cursor IS
+        SELECT e.last_name AS emp, m.last_name AS mng
+        FROM employee e
+        JOIN employee m ON m.employee_id = e.manager_id;
+BEGIN
+    OPEN emp_cursor;
+    FETCH emp_cursor INTO emp_name, manager_name;
+    
+    WHILE emp_cursor%FOUND LOOP
+        dbms_output.put_line(emp_name || ' | ' || manager_name);
+        FETCH emp_cursor INTO emp_name, manager_name;
+    END LOOP;
+    
+    CLOSE emp_cursor;
+END;
+/
+
+
+--ejercicio25
+declare
+    dpt_id number;
+    counter number := 1;
+begin
+    dpt_id := &department_id;
+    for i in (select * from employee where department_id = dpt_id) loop
+        dbms_output.put_line(counter || ': ' || i.last_name);
+        counter := counter + 1;
+    end loop;
+end;
+/
+
+--ejercicio27
+
+
+
+
+
+
+
+
+
+
+select * from temp;
+
+
 
 
 
