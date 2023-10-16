5cad943a76eea4397e3151e920611b8d7a8726a6
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2023-10-16 12:57:17 +0200

    pmdm: expando proyecto RockPaperScissors

--- PMDM/ProyectosAndroidStudio/u2/RockPaperScissors/app/src/main/java/com/rittz/rockpaperscissors/MainActivity.java
+++ PMDM/ProyectosAndroidStudio/u2/RockPaperScissors/app/src/main/java/com/rittz/rockpaperscissors/MainActivity.java
@@ -3,14 +3,21 @@ package com.rittz.rockpaperscissors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int INITIAL_SCORE = 0;
     Button buttonRestart;
+    ImageView imageViewRock, imageViewPaper, imageViewScissors, imageViewChoiceCpu;
+    TextView textViewScoreUser, textViewScoreCpu, textViewCpuChoice, textViewFinalMsg;
 
-    TextView textViewUser, textViewCPU;
+    public enum Selection{
+        ROCK, PAPER, SCISSORS;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonRestart = findViewById(R.id.buttonRestart);
+
+        imageViewRock = findViewById(R.id.imageViewRock);
+        imageViewPaper = findViewById(R.id.imageViewPaper);
+        imageViewScissors = findViewById(R.id.imageViewScissors);
+        imageViewChoiceCpu = findViewById(R.id.imageViewCpuChoice);
+
+        textViewScoreUser = findViewById(R.id.textViewScoreUser);
+        textViewScoreCpu = findViewById(R.id.textViewScoreCpu);
+        textViewCpuChoice = findViewById(R.id.textViewCpuChoice);
+        textViewFinalMsg = findViewById(R.id.textViewFinalMsg);
+
+        textViewCpuChoice.setVisibility(View.GONE);
+
+        textViewScoreUser.setText(INITIAL_SCORE);
+        textViewScoreCpu.setText(INITIAL_SCORE);
+
+        View.OnClickListener RockPaperScissorsHandler = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        }
+
+        imageViewRock.setOnClickListener(new V);
+
+
+
+
+
+
     }
 }
