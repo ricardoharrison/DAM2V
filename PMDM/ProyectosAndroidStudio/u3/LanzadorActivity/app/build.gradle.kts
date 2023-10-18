5e6f2ff23f9f7c423b08e9b1d25dc225948a0590
at1DAM2 <at1DAM2@informatica.iesjc>
2023-10-18 21:37:12 +0200

    pmdm: cambios en ejercicio LanzadorActivity

--- PMDM/ProyectosAndroidStudio/u3/LanzadorActivity/app/build.gradle.kts
+++ PMDM/ProyectosAndroidStudio/u3/LanzadorActivity/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.rittz.lanzadoractivity"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.rittz.lanzadoractivity"
