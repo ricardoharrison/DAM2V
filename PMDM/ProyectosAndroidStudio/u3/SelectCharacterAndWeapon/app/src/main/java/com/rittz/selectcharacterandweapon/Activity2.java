be432b1189524918c8a67003f820d02886d03512
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2023-11-05 14:52:17 +0100

    pmdm: avance en proyecto SelectCharacterAndWeapon

--- PMDM/ProyectosAndroidStudio/u3/SelectCharacterAndWeapon/app/src/main/java/com/rittz/selectcharacterandweapon/Activity2.java
+++ PMDM/ProyectosAndroidStudio/u3/SelectCharacterAndWeapon/app/src/main/java/com/rittz/selectcharacterandweapon/Activity2.java
@@ -2,13 +2,67 @@ package com.rittz.selectcharacterandweapon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Activity2 extends AppCompatActivity {
+    static final int DEFAULT_VALUE = -1;
+    static final String INFO_SELECTED_CHAR = "Activity2.selectedChar";
+    ImageButton imageButtonCharSel1, imageButtonCharSel2, imageButtonCharSel3, imageButtonCharSel4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        imageButtonCharSel1 = findViewById(R.id.imageButtonCharSel1);
+        imageButtonCharSel2 = findViewById(R.id.imageButtonCharSel2);
+        imageButtonCharSel3 = findViewById(R.id.imageButtonCharSel3);
+        imageButtonCharSel4 = findViewById(R.id.imageButtonCharSel4);
+
+        ArrayList<ImageButton> imageButtonsList = new ArrayList<>();
+        imageButtonsList.add(imageButtonCharSel1);
+        imageButtonsList.add(imageButtonCharSel2);
+        imageButtonsList.add(imageButtonCharSel3);
+        imageButtonsList.add(imageButtonCharSel4);
+
+
+        imageButtonCharSel1.setImageResource(R.drawable.heroe);
+        imageButtonCharSel2.setImageResource(R.drawable.mage);
+        imageButtonCharSel3.setImageResource(R.drawable.elf);
+        imageButtonCharSel4.setImageResource(R.drawable.zombie);
+
+        Intent intent = getIntent();
+        ArrayList<Integer> selectedCharacterImages = intent.getIntegerArrayListExtra(MainActivity.INFO_SELECTED_CHAR_IMAGES);
+        Integer selectedPlayer = intent.getIntExtra(MainActivity.INFO_PLAYER_CALLING, DEFAULT_VALUE);
+
+        selectedCharacterImages.set(selectedPlayer, null);
+
+        for(int i = 0; i < selectedCharacterImages.size(); i++){
+            if(i == selectedPlayer){
+                continue;
+            } else {
+                imageButtonsList.get(i).setClickable(false);
+            }
+        }
+
+
+        View.OnClickListener handler = view -> {
+            Toast.makeText(Activity2.this, "Clicked", Toast.LENGTH_LONG).show();
+            if(!selectedCharacterImages.contains(view.getId())){
+                selectedCharacterImages.set(selectedPlayer, view.getId());
+                Intent data = new Intent();
+                data.putExtra(INFO_SELECTED_CHAR, view.getId());
+                setResult(Activity2.RESULT_OK);
+                finish();
+            }
+        };
+
+
     }
 }
--- PMDM/ProyectosAndroidStudio/u3/SelectCharacterAndWeapon/app/src/main/java/com/rittz/selectcharacterandweapon/MainActivity.java
+++ PMDM/ProyectosAndroidStudio/u3/SelectCharacterAndWeapon/app/src/main/java/com/rittz/selectcharacterandweapon/MainActivity.java
@@ -11,16 +11,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+
 enum SelectionType {
     CHARACTER, WEAPON;
 }
 public class MainActivity extends AppCompatActivity {
+    static final int TOTAL_PLAYERS = 2;
+    static final int PLAYER1_TAG_NUMBER = 0, PLAYER2_TAG_NUMBER = 1;
+    static final String INFO_PLAYER_CALLING = "MainActivity.playerCalling";
     ImageButton imageButtonCharP1, imageButtonCharP2, imageButtonWeapP1, imageButtonWeapP2;
+
+    final static String INFO_SELECTED_CHAR_IMAGES = "MainActivity.selectedCharacterImages";
+    ArrayList<Integer> selectedCharacterImages = new ArrayList<>();
+    final static String INFO_SELECTED_WEAP_IMAGES = "MainActivity.selectedWeaponImages";
+    ArrayList<Integer> selectedWeaponImages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectedCharacterImages.add(PLAYER1_TAG_NUMBER, null);
+        selectedCharacterImages.add(PLAYER1_TAG_NUMBER, null);
 
         imageButtonCharP1 = findViewById(R.id.imageButtonCharP1);
         imageButtonCharP2 = findViewById(R.id.imageButtonCharP2);
@@ -57,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             public void onActivityResult(ActivityResult result) {
                 if(result.getResultCode() == Activity3.RESULT_OK){
                     Intent data = result.getData();
+                    data.putIntegerArrayListExtra(INFO_SELECTED_WEAP_IMAGES, selectedWeaponImages);
                     //[ViewVaribaleName].setText(data.getStringExtra([ActivityName].[CONSTANT]));
                 } if (result.getResultCode() == Activity3.RESULT_CANCELED){
 
@@ -70,9 +84,13 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener handler = view -> {
             if(view.getTag() == SelectionType.CHARACTER){
                 Intent intent = new Intent(MainActivity.this, Activity2.class);
+                intent.putExtra(INFO_SELECTED_CHAR_IMAGES, selectedCharacterImages);
+                intent.putExtra(INFO_PLAYER_CALLING, PLAYER1_TAG_NUMBER);
                 launcherActivity2.launch(intent);
             } else if (view.getTag() == SelectionType.WEAPON){
                 Intent intent = new Intent(MainActivity.this, Activity3.class);
+                intent.putExtra(INFO_SELECTED_WEAP_IMAGES, selectedWeaponImages);
+                intent.putExtra(INFO_PLAYER_CALLING, PLAYER2_TAG_NUMBER);
                 launcherActivity3.launch(intent);
             }
         };
