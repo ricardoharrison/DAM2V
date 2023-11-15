fa5a084112644458c1b0c717561a1c020e7cf29f
at1DAM2 <at1DAM2@informatica.iesjc>
2023-11-15 17:43:53 +0100

    pmdm: añado practica MyFirstFragment

---
+++ PMDM/ProyectosAndroidStudio/u4/Fragments/MyFirstFragment/app/src/main/java/com/rittz/myfirstfragment/Fragment1.java
@@ -0,0 +1,78 @@
+package com.rittz.myfirstfragment;
+
+import static android.provider.MediaStore.Video.VideoColumns.COLOR_RANGE;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment1 newInstance(String param1, String param2) {
+        Fragment1 fragment = new Fragment1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    TextView textViewColor;
+    static final int COLOR_RANGE = 256;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_1, container, false);
+
+        textViewColor = layout.findViewById(R.id.TextViewColors);
+
+        textViewColor.setOnClickListener(view -> {
+            textViewColor.setBackgroundColor(Color.argb((int)(Math.random() * COLOR_RANGE), (int)(Math.random() * COLOR_RANGE), (int) (Math.random() * COLOR_RANGE), (int) (Math.random() * COLOR_RANGE)));
+        });
+
+        return layout;
+    }
+}
---
+++ PMDM/ProyectosAndroidStudio/u4/Fragments/MyFirstFragment/app/src/main/java/com/rittz/myfirstfragment/Fragment2.java
@@ -0,0 +1,80 @@
+package com.rittz.myfirstfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment2#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment2 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment2.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment2 newInstance(String param1, String param2) {
+        Fragment2 fragment = new Fragment2();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    TextView textViewNumber;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_2, container, false);
+
+        textViewNumber = layout.findViewById(R.id.textViewNumber);
+
+        textViewNumber.setText("0");
+
+        textViewNumber.setOnClickListener(view -> {
+            String text = textViewNumber.getText().toString();
+            int num = Integer.parseInt(text);
+            num += 1;
+            textViewNumber.setText(num);
+        });
+
+        return layout;
+    }
+}
