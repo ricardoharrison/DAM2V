3d11767ac4127828d8d9d6126ff62b8d3e0f959f
at1DAM2 <at1DAM2@informatica.iesjc>
2023-11-22 17:13:23 +0100

    psp: modifico ejercicio CarreraNotifyAll

--- PSP/Prácticas/u2/Threads/CarreraCorredor.java
+++ PSP/Prácticas/u2/Threads/CarreraCorredor.java
@@ -5,30 +5,34 @@ public class CarreraCorredor implements Runnable {
     static final int DISTANCIA_TOTAL = 100, INTERVALO = 10, NUM_CORREDORES = 10;
     private static final long TIEMPO_ESPERA = 500;
     Object lock;
+    Object lock2;
 
-    public CarreraCorredor(int num, Object lock) {
+    public CarreraCorredor(int num, Object lock, Object lock2) {
         this.num = num;
         this.lock = lock;
+        this.lock2 = lock2;
         this.distanciaRecorrida = 0;
     }
 
     public void run(){
-        synchronized (lock) {
-            try {
+        try{
+            synchronized (lock) {
                 lock.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        long inicio = System.currentTimeMillis();
-        while(distanciaRecorrida < DISTANCIA_TOTAL){
-            distanciaRecorrida += (int)(Math.random() * INTERVALO);
-            try {
                 Thread.sleep(TIEMPO_ESPERA);
-            } catch (InterruptedException e) {}
+            }
+            long inicio = System.currentTimeMillis();
+            while(distanciaRecorrida < DISTANCIA_TOTAL){
+                distanciaRecorrida += (int)(Math.random() * INTERVALO);
+                Thread.sleep(TIEMPO_ESPERA);
+            }
+            long llegada = System.currentTimeMillis();
+            synchronized (lock2) {
+                lock2.notify();
+            }
+            tiempo = llegada - inicio;
+            System.out.printf("Soy el corredor nº %d y he llegado a la meta en %d milisegundos.\n", num, tiempo);
+        } catch (InterruptedException e) {
+
         }
-        long llegada = System.currentTimeMillis();
-        tiempo = llegada - inicio;
-        System.out.printf("Soy el corredor nº %d y he llegado a la meta en %d milisegundos.\n", num, tiempo);
     }
 }
--- PSP/Prácticas/u2/Threads/CarreraNotifyAll.java
+++ PSP/Prácticas/u2/Threads/CarreraNotifyAll.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * CarreraNotifyAll
  */
@@ -5,15 +7,25 @@ public class CarreraNotifyAll {
 
     public static void main(String[] args) {
         Object lock = new Object();
+        Object lock2 = new Object();
+        ArrayList<Thread> lista = new ArrayList<>();
         for (int i = 0; i < CarreraCorredor.NUM_CORREDORES; i++) {
-            Thread thread = new Thread(new CarreraCorredor(i + 1, lock));
+            Thread thread = new Thread(new CarreraCorredor(i + 1, lock, lock2));
+            lista.add(thread);
             thread.start();
         }
-        try{
-            Thread.sleep(1000);
-        } catch (InterruptedException e) { }
         synchronized (lock) {
             lock.notifyAll();
+        }
+        synchronized(lock2) {
+            try{
+                lock2.wait();
+            } catch (InterruptedException e) {}            
+        }
+        for(Thread thread : lista){
+            if(thread.isAlive()){
+                thread.interrupt();
+            }
         }
     }
 }
