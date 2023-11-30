72732ae4f10f80ca46de6b2df88b37d741fd9aa9
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2023-11-30 19:13:45 +0100

    psp: subo solucion ejercico 1 examen

---
+++ PSP/Examenes/SolucionExamen1/Md5sumPipe.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Md5sumPipe {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Uso: java MD5Sum <palabra>");
+            return;
+        }
+
+        String palabra = args[0];
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", "echo -ne " + palabra + " | md5sum");
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String md5Sum = reader.readLine();
+
+            System.out.println("MD5Sum de '" + palabra + "': " + md5Sum.split(" ")[0]);
+
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
