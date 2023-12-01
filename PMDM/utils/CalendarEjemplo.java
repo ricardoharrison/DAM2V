437e1900dd5fb96be13acf97d8a9bb04262fbe2b
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2023-12-01 13:50:42 +0100

    pmdm: incluyo ejemplo conciso de DatePicker y TimePicker

---
+++ PMDM/utils/CalendarEjemplo.java
@@ -0,0 +1,29 @@
+import java.util.Calendar;
+
+/**
+ * Test
+ */
+public class CalendarEjemplo {
+
+    public static void main(String[] args) {
+        Calendar ahora = Calendar.getInstance();    //crea instancia con la fecha y hora actual
+
+        Calendar luego = Calendar.getInstance();    //crea instancia de fecha y hora actual para después actualizarla con los datos de una fecha futura/pasada
+        luego.set(Calendar.MONTH, Calendar.MARCH);    
+        luego.set(Calendar.DAY_OF_MONTH, 20);
+        luego.set(Calendar.YEAR, 2035);
+        luego.set(Calendar.HOUR_OF_DAY, 12);    //importante usar esta ya que es basada en 24h
+        luego.set(Calendar.MINUTE, 30);
+        luego.set(Calendar.SECOND, 0);
+
+        System.out.println(ahora.getTime());  //imprime el objeto Calendar en formato abreviado
+        System.out.println(luego.getTime());
+
+        //comparar fechas (usando getTimeInMillis)
+        if(ahora.getTimeInMillis() < luego.getTimeInMillis()){
+            System.out.println(ahora.getTime() + " es antes que " + luego.getTime());
+        } else {
+            System.out.println("...Oops");
+        }
+    }
+}
