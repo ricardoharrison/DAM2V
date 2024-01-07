c8f01a5e61c4cf058c00ed4c8ede7be73a2054f5
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2024-01-07 16:08:18 +0100

    psp: inicio Tarea1

---
+++ PSP/Trimestre2/Practicas/u3/Tarea1/Client1.java
@@ -0,0 +1,45 @@
+package Tarea1;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Client1 {
+    private static final int MAX_LENGTH = (int)(Math.pow(2, 14));
+
+    public static void main(String[] args) {
+        if(args.length != 3){
+            System.out.println("Número erróneo de parámetros");
+        }
+
+        String ip;
+        int port;
+        String msg;
+
+        try{
+            port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e){
+            System.out.println("Introduce un valor numérico para el puerto");
+        }
+
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress ipAddress = InetAddress.getByName(args[0]); // Dirección del servidor
+            byte[] sentData = new byte[MAX_LENGTH];
+            String sentence = args[2]; // Mensaje a enviar
+            
+
+            //TO-DO
+            
+            sentData = sentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sentData, sentData.length, ipAddress, PORT);
+            socket.send(sendPacket); // Envía el paquete al servidor
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        
+    }
+}
---
+++ PSP/Trimestre2/Practicas/u3/Tarea1/Server1.java
@@ -0,0 +1,40 @@
+package Tarea1;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Server1 {
+
+    private static final int MAX_LENGTH = (int)(Math.pow(2, 14));
+
+    public static void main(String[] args) {
+        if(args.length != 1){
+            System.out.println("Se necesita un parámetro");
+            return;
+        }
+
+        int port = 0;
+
+        try{
+            port = Integer.parseInt(args[0]); 
+        } catch (NumberFormatException e){
+            System.out.println("Error al convertir el parámetro a número");
+        }
+
+        try{
+            DatagramSocket socket = new DatagramSocket(port);
+            byte[] receivedData = new byte[MAX_LENGTH];
+
+            while(true){
+                DatagramPacket packet = new DatagramPacket(receivedData, receivedData.length);
+                socket.receive(packet);
+
+                String msg = new String(packet.getData(), 0, packet.getLength());
+                System.out.println(msg);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }     
+    }
+}
