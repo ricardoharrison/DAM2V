d4c884ded05cff2c30ca99f502fd76cd29a6c525
at1DAM2 <at1DAM2@informatica.iesjc>
2024-01-08 18:05:13 +0100

    psp: finalizado Ascensor (básico)

--- PSP/Trimestre2/Practicas/u3/Ascensor/ControlRoom.java
+++ PSP/Trimestre2/Practicas/u3/Ascensor/ControlRoom.java
@@ -1,5 +1,70 @@
 package Ascensor;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class ControlRoom {
-    
+
+    private static final int MAX_LENGTH = (int) (Math.pow(2, 14));
+
+    public static void main(String[] args) {
+
+        int port = 8000;
+        String server = "localhost";
+
+        JFrame frame = new JFrame("Control room");
+
+        JPanel panel = new JPanel();
+
+        frame.setResizable(true);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JLabel label1 = new JLabel();
+        JLabel separator = new JLabel("|\n");
+        JLabel label2 = new JLabel();
+
+        panel.add(label1);
+        panel.add(separator);
+        panel.add(label2);
+
+        frame.add(panel);
+
+        Lift lift1 = new Lift(1, port, server);
+        Lift lift2 = new Lift(2, port, server);
+
+        try {
+            DatagramSocket socket = new DatagramSocket(port);
+            byte[] receivedData = new byte[MAX_LENGTH];
+
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(receivedData, receivedData.length);
+                socket.receive(packet);
+
+                String msg = new String(packet.getData(), 0, packet.getLength());
+                String[] splitMsg = msg.split(";");
+                switch (splitMsg[0]) {
+                    case "1":
+                        label1.setText("Lift No. " + splitMsg[0] + " is currently " + splitMsg[2] + " at/to floor "
+                                + splitMsg[1]);
+                        break;
+                    case "2":
+                        label2.setText("Lift No. " + splitMsg[0] + " is currently " + splitMsg[2] + " at/to floor "
+                                + splitMsg[1]);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- PSP/Trimestre2/Practicas/u3/Ascensor/Lift.java
+++ PSP/Trimestre2/Practicas/u3/Ascensor/Lift.java
@@ -1,98 +1,123 @@
 package Ascensor;
 
+import java.awt.Color;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.time.Duration;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class Lift {
-    static final int TRAVELLING_TIME = 1500;
+    static final int TRAVELLING_TIME = 1900;
     static final int LAPSE = 100;
-    static final int MAX_LENGTH = (int)(Math.pow(2, 14));
+    static final int MAX_LENGTH = (int) (Math.pow(2, 14));
     String server;
     int port;
     int code;
     int floor = 0;
     LiftState direction = LiftState.STATIONARY;
     String infoMsg = "CURRENTLY AT FLOOR ";
+    JLabel labelInfo;
 
-    public Lift(String server, int code, int port) {
+    public Lift(int code, int port, String server) {
         this.code = code;
         this.port = port;
+        this.server = server;
         start();
     }
 
     private void start() {
-        JFrame frame = new JFrame("Lift No. " + code);
-        JPanel panel = new JPanel();
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Lift No. " + code);
+            JPanel panel = new JPanel();
 
-        frame.setResizable(true);
-        frame.setSize(500, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+            frame.setResizable(true);
+            frame.setSize(500, 500);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel labelInfo = new JLabel(infoMsg + floor);
-        JButton buttonUp = new JButton("Go up");
-        JButton buttonDown = new JButton("Go down");
-        
-        buttonUp.addActionListener(li -> {
+            labelInfo = new JLabel(infoMsg + floor);
+            JButton buttonUp = new JButton("Go up");
+            JButton buttonDown = new JButton("Go down");
+
+            buttonUp.addActionListener(li -> {
+                goUp();
+            });
+
+            buttonDown.addActionListener(li -> {
+                goDown();
+            });
+
+            panel.add(buttonUp);
+            panel.add(buttonDown);
+            panel.add(labelInfo);
+
+            frame.add(panel);
+            frame.setVisible(true);
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        DatagramSocket socket = new DatagramSocket();
+                        InetAddress ipAddress = InetAddress.getByName(server);
+                        byte[] sentData = new byte[MAX_LENGTH];
+                        String sentence = code + ";" + floor + ";" + direction.toString();
+
+                        sentData = sentence.getBytes();
+
+                        DatagramPacket sendPacket = new DatagramPacket(sentData, sentData.length, ipAddress, port);
+                        socket.send(sendPacket);
+                        socket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        Thread.sleep(LAPSE);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        });
+    }
+
+    void goUp() {
+        if (direction == LiftState.STATIONARY) {
             floor++;
             direction = LiftState.GOING_UP;
             labelInfo.setText("Going up...");
-            try{
-                Thread.sleep(TRAVELLING_TIME);
-            } catch (InterruptedException e) {}
-            direction = LiftState.STATIONARY;
-            labelInfo.setText(infoMsg + floor);
-        });
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(TRAVELLING_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                direction = LiftState.STATIONARY;
+                labelInfo.setText(infoMsg + floor);
+            });
+            thread.start();
+        }
+    }
 
-        buttonDown.addActionListener(li -> {
+    void goDown() {
+        if (direction == LiftState.STATIONARY) {
             floor--;
             direction = LiftState.GOING_DOWN;
             labelInfo.setText("Going down...");
-            try{
-                Thread.sleep(1500);
-            } catch (InterruptedException e) {}
-            direction = LiftState.STATIONARY;
-            labelInfo.setText(infoMsg + floor);
-        });
-
-        panel.add(buttonUp);
-        panel.add(buttonDown);
-        panel.add(labelInfo);
-
-        frame.add(panel);
-
-        while(true){
-
-            try {
-                DatagramSocket socket = new DatagramSocket();
-                InetAddress ipAddress = InetAddress.getByName(server); // Dirección del servidor
-                byte[] sentData = new byte[MAX_LENGTH];
-                String sentence = code + ";" + floor + ";" + direction.toString(); // Mensaje a enviar
-                
-                sentData = sentence.getBytes();
-
-                DatagramPacket sendPacket = new DatagramPacket(sentData, sentData.length, ipAddress, port);
-                socket.send(sendPacket); // Envía el paquete al servidor
-                socket.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            try {
-                Thread.sleep(LAPSE);
-            } catch (InterruptedException e) {}
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(TRAVELLING_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                direction = LiftState.STATIONARY;
+                labelInfo.setText(infoMsg + floor);
+            });
+            thread.start();
         }
-        
-        
     }
-
-
-
 }
