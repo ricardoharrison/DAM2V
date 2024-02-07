29066d43496eb75980b512cbc9ad83df43fea0bf
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2024-02-07 15:37:19 +0100

    psp: añado ejercicio TCP enviar comando a cliente

---
+++ PSP/u3/TCP/ServerComando/ClientComando.java
@@ -0,0 +1,31 @@
+//package ServerComando;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientComando {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String msg = "";
+        System.out.println("Pulsa ENTER para solicitar el listado de ficheros:");
+        sc.nextLine();
+        try {
+            Socket socket = new Socket("localhost", 1234);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+
+            msg = in.readUTF();
+
+            System.out.println("Listado de ficheros recibido desde el servidor:");
+            System.out.println("------------------------------------------------");
+            System.out.println(msg);
+            System.out.println("------------------------------------------------");
+
+            in.close(); // Close the output stream
+            socket.close(); // Close the socket
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ PSP/u3/TCP/ServerComando/ServerComando.java
@@ -0,0 +1,47 @@
+//package ServerComando;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerComando {
+    public static void main(String[] args) {
+        ServerSocket server;
+        try {
+            server = new ServerSocket(1234);
+            while (true) {
+                // Espera cliente
+                System.out.println("Servidor preparado.");
+                Socket socket = server.accept();
+
+                ProcessBuilder builder = new ProcessBuilder("dir", "C:\\");
+                Process process = builder.start();
+
+                InputStream stream = process.getInputStream();
+                String msg = "";
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        msg += line + "\n"; // hace falta el salto de línea ya que el BufferedReader lo consume
+                    }
+                } catch (Exception e) {
+                }
+
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                out.writeUTF(msg);
+                System.out.println("Mensaje enviado a cliente.");
+
+                socket.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
