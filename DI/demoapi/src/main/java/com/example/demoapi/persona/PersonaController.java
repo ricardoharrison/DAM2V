e963b2ee8a712f98cf7dde69f7f7ee81dca36790
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2024-02-15 19:58:00 +0100

    di: añado nuevas funcionalidades a la API Spring

--- DI/demoapi/src/main/java/com/example/demoapi/persona/PersonaController.java
+++ DI/demoapi/src/main/java/com/example/demoapi/persona/PersonaController.java
@@ -2,11 +2,21 @@ package com.example.demoapi.persona;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController // publica los endpoints (urls con peticiones)
 @RequestMapping("/persona")
@@ -14,11 +24,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class PersonaController {
 
+    @Autowired
+
     private final PersonaService personaService;
 
-    @PostMapping("/crear") // http://localhost:8080/persona/crear
-    public void insertarPersona(@RequestBody Persona persona) {
+    @PostMapping("crear") // http://localhost:8080/persona/crear
+    public ResponseEntity<String> insertarPersona(@RequestBody Persona persona) {
         personaService.crearPersona(persona);
+        return ResponseEntity.ok("Se ha creado la persona con éxito"); // para dar una respuesta al usuario que hace la
+                                                                       // petición
+    }
+
+    @PutMapping("borrarPersona/{id}")
+    public ResponseEntity<String> borrarPersona(@PathVariable String id) {
+        // TODO: process PUT request
+        personaService.borrarPersona(Integer.parseInt(id));
+        return ResponseEntity.ok("Se ha borrado la persona con éxito");
+    }
+
+    @GetMapping("obtenerPersona/{id}")
+    public ResponseEntity<Optional<Persona>> obtenerPersona(@PathVariable String id) {
+        return ResponseEntity.ok(personaService.obtenerPersona(Integer.parseInt(id)));
+    }
+
+    @GetMapping("obtenerTodas")
+    public ResponseEntity<List<Persona>> obtenerTodas() {
+        return ResponseEntity.ok(personaService.obtenerTodas());
     }
 
 }
--- DI/demoapi/src/main/java/com/example/demoapi/persona/PersonaService.java
+++ DI/demoapi/src/main/java/com/example/demoapi/persona/PersonaService.java
@@ -1,5 +1,8 @@
 package com.example.demoapi.persona;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +16,17 @@ public class PersonaService {
 
     public void crearPersona(Persona persona) {
         personaRepository.save(persona);
+    }
+
+    public void borrarPersona(Integer id) {
+        personaRepository.deleteById(id);
+    }
+
+    public Optional<Persona> obtenerPersona(Integer id) {
+        return personaRepository.findById(id);
+    }
+
+    public List<Persona> obtenerTodas() {
+        return personaRepository.findAll();
     }
 }
