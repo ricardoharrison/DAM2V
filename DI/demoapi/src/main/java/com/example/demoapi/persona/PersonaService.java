3ff2254ce3771ce229a19afd30c486763771798e
ricardoharrison <122381594+ricardoharrison@users.noreply.github.com>
2024-02-17 21:22:51 +0100

    di: guardando cambios Spring

--- DI/demoapi/src/main/java/com/example/demoapi/persona/PersonaService.java
+++ DI/demoapi/src/main/java/com/example/demoapi/persona/PersonaService.java
@@ -1,8 +1,5 @@
 package com.example.demoapi.persona;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +13,5 @@ public class PersonaService {
 
     public void crearPersona(Persona persona) {
         personaRepository.save(persona);
-    }
-
-    public void borrarPersona(Integer id) {
-        personaRepository.deleteById(id);
-    }
-
-    public Optional<Persona> obtenerPersona(Integer id) {
-        return personaRepository.findById(id);
-    }
-
-    public List<Persona> obtenerTodas() {
-        return personaRepository.findAll();
     }
 }
