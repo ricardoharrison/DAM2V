dc0098db2a740dc3416912de5eb3c9e31b1091c1
at1DAM2 <at1DAM2@informatica.iesjc>
2024-02-21 16:05:49 +0100

    psp: añado ejercicio ServerPdf

--- PSP/u3/TCP/EnviarPdf/ClientPdf.java
+++ PSP/u3/TCP/EnviarPdf/ClientPdf.java
@@ -1,34 +1,29 @@
-package TCP.EnviarPdf;
+//package TCP.EnviarPdf;
 
 import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.Socket;
 
 public class ClientPdf {
+    static int DATABYTE = 1024;
+    private static String file = "D:\\Perfiles\\at1DAM2\\Desktop\\DAM2V\\PSP\\u3\\TCP\\EnviarPdf\\resultado.pdf";
+
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 1234);
+            Socket socket = new Socket("127.0.0.1", 1234);
+            byte[] pdf = new byte[DATABYTE];
+            int bytesRead;
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            FileOutputStream outputStream = new FileOutputStream(file);
 
-            InputStream inputStream = socket.getInputStream();
-            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            while ((bytesRead = in.read(pdf)) != -1) {
+                outputStream.write(pdf, 0, bytesRead);
+            }
 
-            // Read the length of the file first
-            int fileSize = dataInputStream.readInt();
-            // Read the PDF file into a byte array
-            byte[] buffer = new byte[fileSize];
-            dataInputStream.readFully(buffer, 0, fileSize);
-
-            // Save the received PDF file in the same folder as the client
-            String fileName = "received_file.pdf";
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-            fileOutputStream.write(buffer);
-            fileOutputStream.close();
-
-            System.out.println("File received and saved as: " + fileName);
-
-            socket.close(); // Close the socket
+            in.close();
+            socket.close();
+            outputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
