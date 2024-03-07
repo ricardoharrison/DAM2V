a7c204321a2ea036780b2086fd97871ed50917a4
at1DAM2 <at1DAM2@informatica.iesjc>
2024-03-07 16:41:00 +0100

    zip examen1 android

--- PMDM/Trimestre1/Examen/Examen1/EjerccicioExamen/app/src/main/java/com/rittz/examen/Ciudades.java
+++
@@ -1,7 +0,0 @@
-package com.rittz.examen;
-
-import java.io.Serializable;
-
-public enum Ciudades implements Serializable {
-    MADRID, BARCELONA, VALENCIA, ZARAGOZA, SEVILLA, MALAGA, MURCIA, PALMA, LASPALMAS, ALICANTE;
-}
